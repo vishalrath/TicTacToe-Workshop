51467fe09d8ca597eb5aac286d8a9e8df2f937e2
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-17 12:28:08 +0530

    {vishal]Game Uc1 done

---
+++ TicTacToeGameWork/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TicTacToeGameWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TicTacToeGame obj = new TicTacToeGame();
+            obj.CreateBoard();
+
+        }
+    }
+}
---
+++ TicTacToeGameWork/TicTacToeGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGameWork
+{
+    class TicTacToeGame
+    {
+        public void CreateBoard()
+        {
+            char[] board = new char[10] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+
+            for (int i = 0; i < board.Length; i++)
+            {
+                Console.WriteLine(board[i]);
+            }
+        }
+    }
+}
